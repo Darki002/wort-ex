0c88d2963b9114a55eda2df62dc8dfc6bb74093d
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-01-06 15:39:44 +0100

    basic layout

---
+++ src/WortEx.Frontend/Theme.cs
@@ -0,0 +1,37 @@
+﻿using MudBlazor;
+
+namespace WortEx.Frontend;
+
+public static class Theme
+{
+    public static MudTheme DefaultTheme => GetDefaultTheme();
+    
+    private static MudTheme GetDefaultTheme()
+    {
+        var primaryColor = "#643b96";
+        var darkColor = "#1a1a1a"; // You can customize this color for the dark background
+
+        var mudTheme = new MudTheme()
+        {
+            Palette = new PaletteDark()
+            {
+                Primary = primaryColor,
+                Background = darkColor,
+                DrawerBackground = darkColor,
+                DrawerText = "rgba(255, 255, 255, 0.7)",
+                AppbarBackground = darkColor,
+                AppbarText = "rgba(255, 255, 255, 0.7)",
+                Success = "#4caf50",
+                Info = "#2196f3",
+                Warning = "#ff9800",
+                Error = "#f44336",
+            },
+            Typography = new Typography()
+            {
+                Default = new() { FontSize = "16px", FontFamily = new []{"Roboto", "Arial", "sans-serif"} }
+            },
+        };
+
+        return mudTheme;
+    }
+}
